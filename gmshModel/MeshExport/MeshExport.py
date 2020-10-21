corrected small error for calculation of refinement functions; updated documentation

gmshModel/MeshExport/MeshExport.py
@@ -1,28 +1,27 @@
 import numpy as np
-import math
 
 def FeapExport(model):
 
 	# nodal transformation for connectivity needed for 2nd order tetrahedron
 	# -> swap last two entries
 	tet_2 = [0 , 1 , 2 , 3 , 4 , 5 , 6 , 7 , 9 , 8]
-	
+
 	# prerequisites - abbreviations for easier access
 	model = model.gmshAPI
 	mesh = model.mesh
 	meshfile = 'drve'
-	
+
 	# necessary information for writing feap mesh file
 	dim = model.getDimension()
 	print("Physical dimension:",dim)
 	meshfile = str(dim)+meshfile
-	
+
 	# get information on nodes
 	nodeTags, nodalCoord, paramCoord = mesh.getNodes(dim,-1,includeBoundary=True)
 	nodeTags,indices = np.unique(nodeTags, return_index=True)
 	nNodes = max(nodeTags)
 	print("Number of nodes:",nNodes)
-	
+
 	# sort nodes and coordinates before writing
 	nodalX = nodalCoord[0::3]
 	nodalY = nodalCoord[1::3]
@@ -30,12 +29,12 @@ def FeapExport(model):
 	nodalX = nodalX[indices]
 	nodalY = nodalY[indices]
 	nodalZ = nodalZ[indices]
-	
+
 	nodalCoord = np.zeros(int(nNodes*3))
 	nodalCoord[0::3] = nodalX
 	nodalCoord[1::3] = nodalY
 	nodalCoord[2::3] = nodalZ
-	
+
 	# get information on elements
 	elemTypes,elemTags,connectivity = mesh.getElements(dim,-1)
 	nElem = 0
@@ -48,11 +47,11 @@ def FeapExport(model):
 			maxNodesPerElem = int(currNodesPerElem)
 	print("Number of elements:", nElem)
 	print("Maximum number of nodes per element:",maxNodesPerElem)
-	
+
 	# get information on materials
 	phyGroup = model.getPhysicalGroups(dim)
 	numberOfMat = len(phyGroup)
-	
+
 	## build database where each element has a material
 	elem2mat = np.zeros((nElem,2),dtype=int)
 	startind = 0
@@ -65,19 +64,19 @@ def FeapExport(model):
 			elem2mat[startind:startind+len(currElemTags[0]),0] = currElemTags[0]
 			elem2mat[startind:startind+len(currElemTags[0]),1] = i+1
 			startind = startind+len(currElemTags[0])
-	
+
 	# write problem type file PROB_XXX with general definitions
 	with open('PROB_'+meshfile,'wt') as outfile:
 		outfile.writelines(str(nNodes)+' '+str(nElem)+' '+str(numberOfMat)+' '+str(dim)+' '+str(dim)+' '+str(maxNodesPerElem)+'\n')
 		outfile.writelines('! nodes, elements, material sets, mesh dimension, maximum nodal DOF (default = dim), max nodes/element')
-				
+
 	# write mesh file MESH_XXX with nodal coordinates and element connectivity for every element type
 	with open('MESH_'+meshfile,'wt') as outfile:
 		outfile.writelines('COORDinate\n')
 		for i in range(0,nNodes):
 			coordStr = np.array2string(nodalCoord[3*i:3*(i+1)],max_line_width=100000)
 			outfile.writelines(str(i+1)+' 0 '+coordStr[1:-1]+'\n')
-			
+
 		totalElemCount = 0
 		for i in range(0,len(elemTypes)):
 			currNodesPerElem = int(len(connectivity[i])/len(elemTags[i]))
@@ -87,7 +86,7 @@ def FeapExport(model):
 			print('Elements of current type:',elemsOfType)
 			outfile.writelines('\n')
 			outfile.writelines('ELEMent NODEs='+str(currNodesPerElem)+'\n')
-			
+
 			# due to limited input records in one line (=16), elements with more
     	# than 13 nodes have to be split into several rows
 			for j in range(0,elemsOfType):
@@ -103,7 +102,6 @@ def FeapExport(model):
 				outfile.writelines(connectStr13[1:-1]+'\n')
 				# writing entries 14 - x of connectivity in new lines
 				# automatic linebreak every 16th entry
-				for cLines in range(0,int(math.ceil(max(currNodesPerElem-13,0)/16))):
+				for cLines in range(0,int(np.ceil(max(currNodesPerElem-13,0)/16))):
 					connectStr14x = np.array2string(currConnectivity[13+cLines*16:min(13+(cLines+1)*16,currNodesPerElem)],max_line_width=100000)
 					outfile.writelines('    '+connectStr14x[1:-1]+'\n')
-				

gmshModel/Model/InclusionRVE.py
@@ -245,26 +245,27 @@ class InclusionRVE(GenericRVE):
         for iInc in range(0,np.shape(incInfo)[0]):
 
             # get current inclusion from original incInfo array and initialize number of instances
-            thisIncInfo=np.atleast_2d(incInfo[iInc,:])
-            thisIncInstances=1
+            thisIncInfo=np.atleast_2d(incInfo[iInc,:])                          # information of original inclusion
+            copyDirs=np.zeros((1,3),dtype=bool)                                 # array to mark copies of the original inclusion in specific directions
+            thisIncInstances=1                                                  # number of instances for this inclusion
 
             # get distance of inclusion to boundaries
             distBndsCenter=np.absolute(self._getDistanceVector(thisIncInfo[0,:],bndPoints,axes)) # calculate (per-direction) distance of inclusion center to domain boundaries
-            distBnds=distBndsCenter-thisIncInfo[0,3]                                              # get corresponding distance of inclusion boundary
-            closeBnds=np.array(np.where((distBnds<=relDistBnd*thisIncInfo[0,3]) & (distBnds>0)))  # check which inclusions are close to which boundaries of the domain; omit inclusions with negative distances to the boundaries, since they are periodic copies of other inclusions and do not need to be checked separately
+            distBnds=distBndsCenter-thisIncInfo[0,3]                                             # get corresponding distance of inclusion boundary
+            closeBnds=np.array(np.where((distBnds<=relDistBnd*thisIncInfo[0,3]) & (distBnds>0))) # check which inclusions are close to which boundaries of the domain; omit inclusions with negative distances to the boundaries, since they are periodic copies of other inclusions and do not need to be checked separately
 
             # loop over all "close" boundaries
             for iBnd in range(0,np.shape(closeBnds)[1]):
-                thisIncCopies=cp.deepcopy(thisIncInfo)                          # initialize current copy with current inclusion data (deepcopy)
-                thisIncCopies[:,axes[closeBnds[1,iBnd]]]+=(-1)**closeBnds[0,iBnd]*self.size[axes[closeBnds[1,iBnd]]] # modify inclusion centers of copies
+                validIncs=~copyDirs[:,axes[closeBnds[1,iBnd]]]                  # get valid inclusions to copy, i.e.: inclusions that have not been copied along this axis before
+                thisIncCopies=cp.deepcopy(thisIncInfo[validIncs,:])             # initialize current copy with current inclusion data (deepcopy)
+                isCopyInDir=cp.deepcopy(copyDirs[validIncs,:])                  # initialize indicator for copies in specific directions
+                thisIncCopies[:,axes[closeBnds[1,iBnd]]]+=(-1)**closeBnds[0,iBnd]*self.size[axes[closeBnds[1,iBnd]]] # modify inclusion centers of copies (only if they are not already a copy in this direction -> prevent superfluous copies)
+                isCopyInDir[:,axes[closeBnds[1,iBnd]]]=True                     # set current axis direction to True (indicate that this already is a copy in the specified direction)
                 thisIncInfo=np.r_[thisIncInfo,thisIncCopies]                    # append copied inclusions to current inclusion data
-
-            # use unique for the unlikely case of inclusions that are close to
-            # positive and negative boundaries (inclusions close to the center)
-            thisIncInfo=np.unique(thisIncInfo,axis=0)                           # original and copies of this inclusion
-            thisIncInstances=np.shape(thisIncInfo)[0]                           # number of instances for this inclusion
+                copyDirs=np.r_[copyDirs,isCopyInDir]                            # append markers to overall array
 
             # update extended incInfo array
+            thisIncInstances=np.shape(thisIncInfo)[0]                           # number of instances for this inclusion
             extIncInfo[totalIncInstances:totalIncInstances+thisIncInstances,:]=thisIncInfo # save information on current inclusion and its copies
             totalIncInstances+=thisIncInstances                                 # updated number of total inclusion instances
 